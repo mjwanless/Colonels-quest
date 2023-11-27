afa39cc6b0c8048fb1b15263e90583a7ce844cc5
Malcolm Wanless <mwanless2@my.bcit.ca>
2023-11-26 19:39:36 -0800

    Fix out of bounds issue in game board logic

--- game.py
+++ game.py
@@ -21,9 +21,11 @@ def make_board():
 	board_dictionary.update({(2, 11): f'You\'re in room ({2}, {11}): There is a door here.'})
 	board_dictionary.update({(2, 17): f'You\'re in room ({2}, {17}): There is a door here.'})
 
-	print("board made")
-	for item in board_dictionary:
-		print(board_dictionary[item])
+	# for row in range(5):
+	# 	for col in range(21):
+	# 		if (row, col) in board_dictionary.keys():
+	# 			print(board_dictionary[(row, col)])
+
 	return board_dictionary
 
 
@@ -32,59 +34,54 @@ def make_character():
 	return {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5}
 
 
-#
-# def describe_current_location(board, character):
-#
-# 	print(board[(character["X-coordinate"], character["Y-coordinate"])])
-#
-#
-# def get_user_choice():
-#
-# 	user_directions = {1: "north", 2: "east", 3: "south", 4: "west"}
-# 	valid_user_input = False
-# 	user_direction_input = 0
-#
-# 	while not valid_user_input:
-# 		user_direction_input = int(input("Please input a direction of either 1. North, 2. East, 3. South, "
-# 		                                 "4. West: "))
-# 		if user_direction_input in user_directions:
-# 			valid_user_input = True
-# 		else:
-# 			print("The direction provided doesn't exist in our system; Please try again.")
-#
-# 	return user_direction_input
-#
-#
-# def validate_move(board, character, direction):
-#
-# 	if direction == 1:
-# 		if (character["X-coordinate"], character["Y-coordinate"] - 1) not in board:
-# 			return False
-# 	elif direction == 2:
-# 		if (character["X-coordinate"] + 1, character["Y-coordinate"]) not in board:
-# 			return False
-# 	elif direction == 3:
-# 		if (character["X-coordinate"], character["Y-coordinate"] + 1) not in board:
-# 			return False
-# 	elif direction == 4:
-# 		if (character["X-coordinate"] - 1, character["Y-coordinate"]) not in board:
-# 			return False
-#
-# 	return True
-#
-#
-# def move_character(character, direction):
-#
-# 	if direction == 1:
-# 		character["Y-coordinate"] -= 1
-# 	elif direction == 2:
-# 		character["X-coordinate"] += 1
-# 	if direction == 3:
-# 		character["Y-coordinate"] += 1
-# 	elif direction == 4:
-# 		character["X-coordinate"] -= 1
-#
-#
+def describe_current_location(board, character):
+	print(board[(character["Y-coordinate"], character["X-coordinate"])])
+
+
+def get_user_choice():
+	user_directions = {1: "north", 2: "east", 3: "south", 4: "west"}
+	valid_user_input = False
+	user_direction_input = 0
+
+	while not valid_user_input:
+		user_direction_input = int(input("Please input a direction of either 1. North, 2. East, 3. South, "
+		                                 "4. West: "))
+		if user_direction_input in user_directions:
+			valid_user_input = True
+		else:
+			print("The direction provided doesn't exist in our system; Please try again.")
+
+	return user_direction_input
+
+
+def validate_move(board, character, direction):
+	if direction == 1:
+		if (character["Y-coordinate"] - 1, character["X-coordinate"]) not in board:
+			return False
+	elif direction == 2:
+		if (character["Y-coordinate"], character["X-coordinate"] + 1) not in board:
+			return False
+	elif direction == 3:
+		if (character["Y-coordinate"] + 1, character["X-coordinate"]) not in board:
+			return False
+	elif direction == 4:
+		if (character["Y-coordinate"], character["X-coordinate"] - 1) not in board:
+			return False
+
+	return True
+
+
+def move_character(character, direction):
+	if direction == 1:
+		character["Y-coordinate"] -= 1
+	elif direction == 2:
+		character["X-coordinate"] += 1
+	if direction == 3:
+		character["Y-coordinate"] += 1
+	elif direction == 4:
+		character["X-coordinate"] -= 1
+
+
 # def check_if_goal_attained(character):
 #
 # 	return(character["X-coordinate"], character["Y-coordinate"]) == (2, 2)
@@ -117,16 +114,17 @@ def game():
 	columns = 3
 	board = make_board()
 	character = make_character()
+	achieved_goal = False
+	while not achieved_goal and character["Current HP"] > 0:
+		describe_current_location(board, character)
+		direction = get_user_choice()
+		valid_move = validate_move(board, character, direction)
+		if valid_move:
+			move_character(character, direction)
+			describe_current_location(board, character)
+		else:
+			print("Nice try, but that direction isn't valid! Please try again.")
 
-
-# achieved_goal = False
-# while not achieved_goal and character["Current HP"] > 0:
-# 	describe_current_location(board, character)
-# 	direction = get_user_choice()
-# 	valid_move = validate_move(board, character, direction)
-# 	if valid_move:
-# 		move_character(character, direction)
-# 		describe_current_location(board, character)
 #       (new version from sample code flow) there_is_a_challenge = check_for_challenges()
 # 		there_is_a_challenger = check_for_foes()
 # 		if there_is_a_challenger: (challenge)
@@ -138,8 +136,7 @@ def game():
 # 			guessing_game(character)
 #       (add board to achieved_goal, like so: achieved_goal = check_if_goal_attained(board, character))
 # 		achieved_goal = check_if_goal_attained(character)
-# 	else:
-# 		print("Nice try, but that direction isn't valid! Please try again.")
+
 #
 # if character["Current HP"] > 0:
 # 	print("Congrats on getting to the end! You win!")
