be41174f1d6aab12f6b0288a2ead99743fe20c48
Malcolm Wanless <mwanless2@my.bcit.ca>
2023-12-09 12:24:17 -0800

    Code docstrings and unit tests for generators.py

--- generators.py
+++ generators.py
@@ -44,10 +44,10 @@ def enemy_generator(enemy):
 	:return: a dictionary that represents an enemy character
 
     >>> enemy_generator("hen")
-    {'name': 'hen', 'Current HP': 15, 'Max HP': 15, 'Exp Value': 2}
+    {'name': 'hen', 'Current HP': 20, 'Max HP': 20, 'Exp Value': 2}
 
     >>> enemy_generator("silkie")
-    {'name': 'silkie', 'Current HP': 30, 'Max HP': 30, 'Exp Value': 3}
+    {'name': 'silkie', 'Current HP': 35, 'Max HP': 35, 'Exp Value': 3}
 
     >>> enemy_generator("rooster")
     {'name': 'rooster', 'Current HP': 50, 'Max HP': 50, 'Exp Value': 5}
@@ -66,6 +66,16 @@ def enemy_generator(enemy):
 
 
 def make_character():
+	"""
+	Generate the character object.
+
+	:postcondition: the returned object represents the character's stats.
+	:return: a dictionary that represents an player character
+
+	>>> make_character()
+	{'X-coordinate': 0, 'Y-coordinate': 2, 'Current HP': 20, 'Max HP': 20, 'Current Mana': 70, 'Max Mana': 70, 'Exp': 0, 'Exp Needed': 15, 'Current Level': 1}
+
+	"""
 	return {
 		"X-coordinate": 0,
 		"Y-coordinate": 2,
@@ -79,16 +89,52 @@ def make_character():
 
 
 def make_hen():
-	return {"name": "hen", "Current HP": 15, "Max HP": 15, "Exp Value": 2}
+	"""
+	Generate the hen object.
+
+	:postcondition: the returned object represents the stats that a hen enemy has.
+	:return: a dictionary that represents a hen enemy
+
+	>>> make_hen()
+	{'name': 'hen', 'Current HP': 20, 'Max HP': 20, 'Exp Value': 2}
+	"""
+	return {"name": "hen", "Current HP": 20, "Max HP": 20, "Exp Value": 2}
 
 
 def make_silkie():
-	return {"name": "silkie", "Current HP": 30, "Max HP": 30, "Exp Value": 3}
+	"""
+	Generate the silkie object.
+
+	:postcondition: the returned object represents the stats that a silkie enemy has.
+	:return: a dictionary that represents a silkie enemy
+
+	>>> make_silkie()
+	{'name': 'silkie', 'Current HP': 35, 'Max HP': 35, 'Exp Value': 3}
+	"""
+	return {"name": "silkie", "Current HP": 35, "Max HP": 35, "Exp Value": 3}
 
 
 def make_rooster():
+	"""
+	Generate the rooster object.
+
+	:postcondition: the returned object represents the stats that a rooster enemy has.
+	:return: a dictionary that represents a rooster enemy
+
+	>>> make_rooster()
+	{'name': 'rooster', 'Current HP': 50, 'Max HP': 50, 'Exp Value': 5}
+	"""
 	return {"name": "rooster", "Current HP": 50, "Max HP": 50, "Exp Value": 5}
 
 
 def make_sanders():
+	"""
+	Generate the sanders boss object.
+
+	:postcondition: the returned object represents the stats that colonel sanders has.
+	:return: a dictionary that represents colonel sanders
+
+	>>> make_sanders()
+	{'name': 'sanders', 'Current HP': 100, 'Max HP': 100, 'Exp Value': 0}
+	"""
 	return {"name": "sanders", "Current HP": 100, "Max HP": 100, "Exp Value": 0}
